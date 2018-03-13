65318f00e8266544829c732502ad79c36d2f2773
NickGatti <nick.gatti@gmail.com>
2018-03-13 14:21:08 -0700

    splash content finished

--- phar-frontend/src/components/splash/BottomContent.js
+++ phar-frontend/src/components/splash/BottomContent.js
@@ -12,11 +12,11 @@ class BottomContent extends React.Component {
                         <img className="img-fluid" src={Zeus} />
                     </Col>
                     <Col xs="4" sm="4" md="4" lg="4" xl="4">
-                        <div>Nothing to read into there! My man! I am Mr. Booby Buyer. I'll buy those boobies for 25 schmeckles! Hello Jerry, come to rub my face in urine again?
+                        <div>You may donate by credit or debit card at <a href="https://www.paypal.com/donate/?token=gyHFvDuny0dkwN3mD7P75g6CxaqlZKjCACJTN1XpnX7K0_ZWATdGGiX01_mBlH8Jzhz6uG">PayPal</a>, or send us a message if you prefer to mail a check and we'll give you the information for that.
 
-Close. It's gonna make your kidneys shut down. It's fine, everythings is fine. theres an infinite number of realities Morty, and in a few dozens of those i got lucky and turned everything back to normal. I took your family? Who do you think had taken more from them when you shot 2 Puh rum pum pow!
+You may also donate via our paypal at Pathwayshomerescue@gmail.com
 
-Then let me GET to know you! Alien Invasion Tomato Monster Mexican Armada Brothers Who Are Just Regular Brothers Running In A Van From An Asteroid And All Sorts Of Things: The Movie. What, I'm gonna touch it, and you're gonna tell me it's an alien dick or something? Are these pills supposed to wake me up or something?</div>
+Thank you so much for caring for the dogs!</div>
                     </Col>
                     <Col xs="4" sm="4" md="4" lg="4" xl="4">
                         <img className="img-fluid" src={Julian} />
--- phar-frontend/src/components/splash/Jumbo.js
+++ phar-frontend/src/components/splash/Jumbo.js
@@ -5,12 +5,16 @@ const Jumbo = (props) => {
     return (
         <div>
             <Jumbotron>
-                <h1 className="display-3">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1 className="display-3">Pathways Home Animal Rescue</h1>
+                <p className="lead">Pathways Home Animal Rescue finds great new homes for shelter, owner surrender, and stray animals found in the Phoenix, Arizona area.</p>
                 <hr className="my-2" />
-                <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
+                    <p>Pathways Home Animal Rescue helps reunite lost pets with their owners, but also rescues dogs from the County shelters to keep them from being euthanized.
+                    After receiving any necessary vet care and being altered, vaccinated, and microchipped, then we find loving homes for the dogs we've rescued from the County shelter, as well as for owner surrenders, and for strays when no owners are found.
+                    All of these wonderful little rescue dogs are waiting to meet you
+                    We are always in need of fosters, volunteers, and people to share our posts -- this is a tremendous help in finding homes for the dogs!
+                    Donations are also so appreciated as there are many times we help dogs who need more vet care than the usual spay/neuter. Your donations help a dog walk better, or remove a tumor that a previous owner should have removed, etc.</p>
                 <p className="lead">
-                    <Button color="primary">Learn More</Button>
+                    <Button color="primary">See adoptions</Button>
                 </p>
             </Jumbotron>
         </div>
