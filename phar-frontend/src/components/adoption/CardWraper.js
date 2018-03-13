c915faf0059a9490cd675238322a89a75c36f9a4
NickGatti <nick.gatti@gmail.com>
2018-03-13 16:44:08 -0700

    cards

--- phar-frontend/src/components/adoption/CardWraper.js
+++ phar-frontend/src/components/adoption/CardWraper.js
@@ -6,33 +6,33 @@ class CardWrapper extends Component {
 
 render() {
         return (
-            <Container style={{ marginTop: 30 }}>
+            <Container style={{ marginBottom: 30 }}>
                 <Row>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
-                    <Col className="mt-3" xs="4" sm="4" md="4" lg="4" xl="4">
+                    <Col className="mt-4" xs="4" sm="4" md="4" lg="4" xl="4">
                         <PetCard />
                     </Col>
                 </Row>
