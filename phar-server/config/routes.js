d4c2a5ed7b6bfa1c2b42420d04c464c0e9c8fcb5
NickGatti <nick.gatti@gmail.com>
2018-03-13 10:06:16 -0700

    routes

--- phar-server/config/routes.js
+++ phar-server/config/routes.js
@@ -2,10 +2,18 @@
 const index = require("../controllers/index.js");
 const users = require("../controllers/users.js");
 const adoptions = require("../controllers/adoptions.js");
+const questions = require("../controllers/questions.js");
 
 module.exports = function(app) {
   app.get("/", index.view);
+
   app.post("/register", users.register)
   app.post("/auth", users.auth)
+
   app.get("/adoptions", adoptions.view)
+
+  app.get("/questions", questions.view)
+  app.post("/questions", questions.create)
+  app.put("/questions", questions.edit)
+  app.delete("/questions", questions.delete)
 };
