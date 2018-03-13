978e85b89b705ca55f395e5f2a2b83cc7f46fe85
NickGatti <nick.gatti@gmail.com>
2018-03-13 10:56:07 -0700

    navbar component

--- phar-frontend/src/index.js
+++ phar-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'bootswatch/dist/materia/bootstrap.css'
+import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux'
