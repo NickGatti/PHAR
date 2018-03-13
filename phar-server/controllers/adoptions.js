06002ff6f0a22a9136bd944e9b67420ba2bbf91f
NickGatti <nick.gatti@gmail.com>
2018-03-12 19:45:58 -0700

    backend api call

--- phar-server/controllers/adoptions.js
+++ phar-server/controllers/adoptions.js
@@ -1,5 +1,11 @@
+const axios = require('axios')
+
 module.exports = {
     view: function (req, res) {
-        res.render("index");
+        async function callAPI() {
+            let response = await axios.get(`https://api.petfinder.com/pet.find?key=${process.env.FETCH_ID_KEY}&animal=dog&location=85032&format=json`)
+            res.json(response.data.petfinder.pets)
+        }
+        callAPI()
     }
 };
--- phar-server/models/adoption.js
+++ phar-server/models/adoption.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 const adoptionSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, unique: true},
-    password: String
+    data: Object
 })
 
 module.exports = mongoose.model('Adoption', adoptionSchema)
