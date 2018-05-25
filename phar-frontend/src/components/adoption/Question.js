ab471ca4461f6f33f19fd6748f4f23e62fb38774
NickGatti <nick.gatti@gmail.com>
2018-05-24 20:29:28 -0700

    questions table added

--- phar-frontend/src/components/adoption/Question.js
+++ phar-frontend/src/components/adoption/Question.js
@@ -3,27 +3,36 @@ import { ListGroupItem, Button, Row, Col, Container } from 'reactstrap'
 
 const Question = (props) => {
     return (
+      <div>
+        <Container>
+          <tbody>
+            <thead>
+                <h4>Questions:</h4>
+            </thead>
+          </tbody>
+        </Container>
       <Container>
         <Row>
-          <Col xs="2" sm="2" md="2" lg="2" xl="2">
+          <Col xs="3" sm="3" md="3" lg="3" xl="3">
             <Button style={{ padding: "3px", paddingLeft: "6px", paddingRight: "6px" }} color="primary">
-              Add!
+              Add
             </Button>
           </Col>
           <Col xs="1" sm="1" md="1" lg="1" xl="1" />
-          <Col xs="2" sm="2" md="2" lg="2" xl="2">
+          <Col xs="3" sm="3" md="3" lg="3" xl="3">
             <Button style={{ padding: "3px", paddingLeft: "6px", paddingRight: "6px" }} color="warning">
-              Change!
+              Edit
             </Button>
           </Col>
         <Col xs="1" sm="1" md="1" lg="1" xl="1"/>
           <Col xs="2" sm="2" md="2" lg="2" xl="2">
             <Button style={{ padding: "3px", paddingLeft: "6px", paddingRight: "6px"  }} color="danger">
-              Delete!
+              Delete
             </Button>
           </Col>
         </Row>
       </Container>
+      </div>
     );
 }
 
